91af9c3643d8e9ec04ce4e1aa7adc0d7b82a18b5
Nolawit Mulugeta <NolawitMulugeta.gmail.com>
2026-09-25 13:31:52 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <main>
+      <h1>Amharic Typeface</h1>
+
+      <p>Design your own Ethiopic typeface.</p>
+
+      <div>
+        <span>ሀ</span>
+        <span>ሁ</span>
+        <span>ሂ</span>
+        <span>ሃ</span>
+        <span>ሄ</span>
+        <span>ህ</span>
+        <span>ሆ</span>
+      </div>
+    </main>
+  );
+}
+
+export default App;
